6763d5d50c618fb1dccdbc4f5d012c6807e46566
iamlogeshwaran <113006185+iamlogeshwaran@users.noreply.github.com>
2022-11-02 16:40:18 +0530

    Add files via upload

---
+++ user authentication/user authentication/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace user_authentication
+{
+    class Program
+    {
+        public static int a = 0;
+        public static string username;
+        public static string password;
+        static void Main(string[] args)
+        {
+            user u = new user();
+            Console.WriteLine("Press '1' for Register page"+"\n"+"Press '2' for Login page");
+             a = int.Parse(Console.ReadLine());
+             if (a == 1)
+             {
+                 Console.WriteLine("Enter your name :");
+                 u.Name = Console.ReadLine();
+                 Console.WriteLine("Your age :");
+                 u.Age = int.Parse(Console.ReadLine());
+                 Console.WriteLine("DOB: (DD-MM-YYYY)");
+                 u.Dob = DateTime.Parse(Console.ReadLine());
+                 Console.WriteLine("Username :");
+                 u.Username = Console.ReadLine();
+                 Console.WriteLine("Password :");
+                 u.Password = Console.ReadLine();
+                 Console.WriteLine("Email address :");
+                 u.Emailadd = Console.ReadLine();
+                 string name = u.Name;
+                 int age = u.Age;
+                 DateTime dob = u.Dob;
+                 username = u.Username;
+                 password = u.Password;
+                 string email = u.Emailadd;
+                 Console.WriteLine(u.ToString());
+                 SqlConnection con = new SqlConnection();
+                 con.ConnectionString = "SERVER=DESKTOP-3MUST40\\SQLEXPRESS;database=userauthentication;integrated security=true;";
+                 con.Open();
+                 SqlCommand comm = new SqlCommand();
+                 comm.Connection = con;
+                 //comm.CommandText = "insert into register values('" + username + "','" + password + "')";
+                 //comm.ExecuteNonQuery();
+                 comm.CommandText = "insert into Regdb values('" + name + "','" + age + "','" + dob + "','" + username + "','" + password + "','" + email + "')";
+                 comm.ExecuteNonQuery();
+                 Console.WriteLine("Registered successfully");
+              
+               
+             }
+            if (a == 2)
+            {
+                Console.WriteLine("Username: ");
+                string Username = Console.ReadLine();
+                Console.WriteLine("Password");
+                string Password = Console.ReadLine();
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "SERVER=DESKTOP-3MUST40\\SQLEXPRESS;database=userauthentication;integrated security=true;";
+                con.Open();
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = con;
+                comm.CommandText = "select passcode from Regdb where username='" + Username + "'";
+                string dbpasscode = (string)comm.ExecuteScalar();
+                if (dbpasscode == Password)
+                {
+                    Console.WriteLine("Login successfull");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Username or password");
+                }
+
+
+            }
+            //passcodeconvertor("Logesh");
+            //var today = DateTime.Today;
+            //var age = today.Year - cal.Year;
+            //Console.WriteLine(age);
+            //DateTime dob = new DateTime(2002,08,02);
+            //TimeSpan ans = DateTime.Now - dob;
+            //Console.WriteLine(ans.Days);
+            
+
+        }
+      
+     
+        
+    }
+}
---
+++ user authentication/user authentication/user.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_authentication
+{
+  public class user
+    {
+        string _name;
+        int _age;
+        DateTime _dob;
+        string _username;
+        string _password;
+        string _emailadd;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+    
+
+        public string Emailadd
+        {
+            get { return _emailadd; }
+            set { _emailadd = value; }
+        }
+
+
+        public string Username
+        {
+            get { return _username; }
+            set {
+                int n = value.Length;
+                if (n < 5 || n > 15)
+                {
+                    throw new Exception("Name max'15' and min '5'");
+                }
+                else if (n >= 5 && n <= 15)
+                {
+                   
+                    _name = value;
+                } _username = value;
+            }
+        }
+        public DateTime Dob
+        {
+            get { return _dob; }
+            set {
+                 
+                _dob = value; }
+        }
+        public  string Name
+        {
+
+            get {return _name; }
+            set
+            {
+                  
+                    int n = value.Length;
+                    if (n < 3 || n > 15)
+                    {
+                        throw new Exception("Name max'15' and min '3'");
+                    }
+                    else if(n>=3&&n<=15)
+                    {
+                        _name = value;
+                    }
+                   
+            }
+             
+        }
+     
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                if (value >= 18 && value <= 60)
+                {
+ 
+                }
+                else
+                {
+                    throw new Exception("Age should above '18' and below '60'");
+                }
+            }
+        }
+
+        public static void passcodeconvertor(string password)
+        {
+            var data = Encoding.ASCII.GetBytes(password);
+            var sha1 = new SHA1CryptoServiceProvider();
+            var sha1data = sha1.ComputeHash(data);
+
+            Console.WriteLine(sha1data);
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + "\n" + "Age: " + Age + "\n" + "DOB: " + Dob + "\n" + "Username: " + Username + "\n" + "password: " + Password + "\n" + "Emailaddress: " + Emailadd;
+        }
+    }
+
+
+}
+
